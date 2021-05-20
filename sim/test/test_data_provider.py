79085c52afc186fc30d597d8cc6566a95716aa48
Jason Lee <Jason.Lee@microsoft.com>
2021-05-20 18:59:19 -0400

    Remove redundant test classes

--- sim/test/test_data_provider.py
+++ sim/test/test_data_provider.py
@@ -14,153 +14,139 @@ valid_db_path = os.path.join(
 )
 
 
-class TestQcodesDataProvider:
-    def test_valid_1d_data_provider(self):
-        """Create a 1D Data provider, and verify that reading from it with
-        different input values yields different outputs.
-        """
+def test_valid_1d_data_provider():
+    """Create a 1D Data provider, and verify that reading from it with
+    different input values yields different outputs.
+    """
 
-        in1 = simulate.Pin("in1")
+    in1 = simulate.Pin("in1")
+    data_1d = data_providers.QcodesDataProvider(
+        [in1], valid_db_path, "GB_Newtown_Dev_3_2", 986
+    )
+
+    in1.set_value(0.0)
+    out0 = data_1d.value
+
+    in1.set_value(-0.5)
+    out1 = data_1d.value
+
+    assert out0 != out1
+
+def test_valid_2d_data_provider():
+    """Create a 2D Data provider, and verify that reading from it with
+    different input values yields different outputs.
+    """
+
+    in1 = simulate.Pin("in1")
+    in2 = simulate.Pin("in2")
+    data_2d = data_providers.QcodesDataProvider(
+        [in1, in2], valid_db_path, "GB_Newtown_Dev_3_2", 991
+    )
+
+    in1.set_value(0.0)
+    in2.set_value(0.0)
+    out0 = data_2d.value
+
+    in1.set_value(-0.5)
+    in2.set_value(-0.5)
+    out1 = data_2d.value
+    assert out0 != out1
+
+def test_bad_db_filename():
+    """Validate the correct error is raised when trying to load a bad
+    database.
+    """
+
+    with pytest.raises(FileNotFoundError):
         data_1d = data_providers.QcodesDataProvider(
-            [in1], valid_db_path, "GB_Newtown_Dev_3_2", 986
+            [], "bad_database.db", "GB_Newtown_Dev_3_2", 991
         )
 
-        in1.set_value(0.0)
-        out0 = data_1d.value
-
-        in1.set_value(-0.5)
-        out1 = data_1d.value
-
-        assert out0 != out1
-
-    def test_valid_2d_data_provider(self):
-        """Create a 2D Data provider, and verify that reading from it with
-        different input values yields different outputs.
-        """
-
-        in1 = simulate.Pin("in1")
-        in2 = simulate.Pin("in2")
-        data_2d = data_providers.QcodesDataProvider(
-            [in1, in2], valid_db_path, "GB_Newtown_Dev_3_2", 991
+def test_dataset_not_found():
+    """Validates the correct error is raised if the specified dataset is
+    not found in the db.
+    """
+    with pytest.raises(NameError):
+        data_1d = data_providers.QcodesDataProvider(
+            [simulate.Pin("in1")],
+            valid_db_path,
+            "Bad_Experiment_Name",
+            986,
         )
 
-        in1.set_value(0.0)
-        in2.set_value(0.0)
-        out0 = data_2d.value
+    with pytest.raises(NameError):
+        data_1d = data_providers.QcodesDataProvider(
+            [simulate.Pin("in1")],
+            valid_db_path,
+            "GB_Newtown_Dev_3_2",
+            987654321,
+        )
 
-        in1.set_value(-0.5)
-        in2.set_value(-0.5)
-        out1 = data_2d.value
-        assert out0 != out1
+def test_good_dataset_parameter():
+    """Validates the data provider uses the specified output parameter from
+    the source dataset.
+    """
 
-    def test_bad_db_filename(self):
-        """Validate the correct error is raised when trying to load a bad
-        database.
-        """
+    data_1d = data_providers.QcodesDataProvider(
+        [simulate.Pin("in1")],
+        valid_db_path,
+        "GB_Newtown_Dev_3_2",
+        986,
+        model_param_name="sr860_2_R_current",
+    )
 
-        with pytest.raises(FileNotFoundError):
-            data_1d = data_providers.QcodesDataProvider(
-                [], "bad_database.db", "GB_Newtown_Dev_3_2", 991
-            )
+    assert data_1d.value != 0.0
 
-    def test_dataset_not_found(self):
-        """Validates the correct error is raised if the specified dataset is
-        not found in the db.
-        """
-        with pytest.raises(NameError):
-            data_1d = data_providers.QcodesDataProvider(
-                [simulate.Pin("in1")],
-                valid_db_path,
-                "Bad_Experiment_Name",
-                986,
-            )
+def test_bad_dataset_parameter():
+    """Validates that the correct error is raised if the specified parameter
+    name is not found in the dataset.
+    """
 
-        with pytest.raises(NameError):
-            data_1d = data_providers.QcodesDataProvider(
-                [simulate.Pin("in1")],
-                valid_db_path,
-                "GB_Newtown_Dev_3_2",
-                987654321,
-            )
-
-    def test_good_dataset_parameter(self):
-        """Validates the data provider uses the specified output parameter from
-        the source dataset.
-        """
-
+    with pytest.raises(KeyError):
         data_1d = data_providers.QcodesDataProvider(
             [simulate.Pin("in1")],
             valid_db_path,
             "GB_Newtown_Dev_3_2",
             986,
-            model_param_name="sr860_2_R_current",
+            model_param_name="BAD_PARAM_NAME",
         )
 
-        assert data_1d.value != 0.0
+def test_too_few_input_bindings():
+    """Validates that the correct error is raised if too few input binds are
+    specified for indexing into the specified dataset.
+    """
 
-    def test_bad_dataset_parameter(self):
-        """Validates that the correct error is raised if the specified parameter
-        name is not found in the dataset.
-        """
+    in1 = simulate.Pin("in1")
 
-        with pytest.raises(KeyError):
-            data_1d = data_providers.QcodesDataProvider(
-                [simulate.Pin("in1")],
-                valid_db_path,
-                "GB_Newtown_Dev_3_2",
-                986,
-                model_param_name="BAD_PARAM_NAME",
-            )
+    with pytest.raises(ValueError):
+        # This is a 2D data set, but only 1 input binding is specified
+        data_1d = data_providers.QcodesDataProvider(
+            [], valid_db_path, "GB_Newtown_Dev_3_2", 986
+        )
 
-    def test_too_few_input_bindings(self):
-        """Validates that the correct error is raised if too few input binds are
-        specified for indexing into the specified dataset.
-        """
+    with pytest.raises(ValueError):
+        # This is a 2D data set, but only 1 input binding is specified
+        data_2d = data_providers.QcodesDataProvider(
+            [in1], valid_db_path, "GB_Newtown_Dev_3_2", 991
+        )
 
-        in1 = simulate.Pin("in1")
+def test_too_many_input_bindings():
+    """Validates that the correct error is raised if too many input binds
+    are specified for indexing into the specified dataset.
+    """
 
-        with pytest.raises(ValueError):
-            # This is a 2D data set, but only 1 input binding is specified
-            data_1d = data_providers.QcodesDataProvider(
-                [], valid_db_path, "GB_Newtown_Dev_3_2", 986
-            )
+    in1 = simulate.Pin("in1")
+    in2 = simulate.Pin("in2")
+    in3 = simulate.Pin("in3")
 
-        with pytest.raises(ValueError):
-            # This is a 2D data set, but only 1 input binding is specified
-            data_2d = data_providers.QcodesDataProvider(
-                [in1], valid_db_path, "GB_Newtown_Dev_3_2", 991
-            )
+    with pytest.raises(ValueError):
+        # This is a 2D data set, but only 1 input binding is specified
+        data_1d = data_providers.QcodesDataProvider(
+            [in1, in2], valid_db_path, "GB_Newtown_Dev_3_2", 986
+        )
 
-    def test_too_many_input_bindings(self):
-        """Validates that the correct error is raised if too many input binds
-        are specified for indexing into the specified dataset.
-        """
-
-        in1 = simulate.Pin("in1")
-        in2 = simulate.Pin("in2")
-        in3 = simulate.Pin("in3")
-
-        with pytest.raises(ValueError):
-            # This is a 2D data set, but only 1 input binding is specified
-            data_1d = data_providers.QcodesDataProvider(
-                [in1, in2], valid_db_path, "GB_Newtown_Dev_3_2", 986
-            )
-
-        with pytest.raises(ValueError):
-            # This is a 2D data set, but only 1 input binding is specified
-            data_2d = data_providers.QcodesDataProvider(
-                [in1, in2, in3], valid_db_path, "GB_Newtown_Dev_3_2", 991
-            )
-
-
-# t = TestQcodesDataProvider()
-# t.test_too_few_input_bindings()
-# t.test_too_many_input_bindings()
-
-# t.test_good_dataset_parameter()
-# t.test_bad_dataset_parameter()
-
-# t.test_dataset_not_found()
-# t.test_valid_1d_data_provider()
-# t.test_valid_2d_data_provider()
-# t.test_bad_db_filename()
+    with pytest.raises(ValueError):
+        # This is a 2D data set, but only 1 input binding is specified
+        data_2d = data_providers.QcodesDataProvider(
+            [in1, in2, in3], valid_db_path, "GB_Newtown_Dev_3_2", 991
+        )
--- sim/test/test_simulator_registry.py
+++ sim/test/test_simulator_registry.py
@@ -3,32 +3,31 @@ from sim.simulate import QuantumDotSim
 from sim.simulator_registry import SimulatorRegistry
 
 
-class TestSimulatorRegistry:
-    def test_default_registration(self):
-        """Validates that a simulator auto-registers with the Simulation Registry"""
+def test_default_registration():
+    """Validates that a simulator auto-registers with the Simulation Registry"""
 
-        qdsim = QuantumDotSim("sim1")
+    qdsim = QuantumDotSim("sim1")
 
-        sim1 = SimulatorRegistry.get("sim1")
-        assert sim1 == qdsim
+    sim1 = SimulatorRegistry.get("sim1")
+    assert sim1 == qdsim
 
-    def test_registration_with_custom_name(self):
-        """Validates that a simulator auto-registers with the Simulation Registry"""
+def test_registration_with_custom_name():
+    """Validates that a simulator auto-registers with the Simulation Registry"""
 
-        qdsim = QuantumDotSim("sim1")
-        SimulatorRegistry.register(qdsim, name="altsim")
+    qdsim = QuantumDotSim("sim1")
+    SimulatorRegistry.register(qdsim, name="altsim")
 
-        sim1 = SimulatorRegistry.get("sim1")
-        altsim = SimulatorRegistry.get("altsim")
+    sim1 = SimulatorRegistry.get("sim1")
+    altsim = SimulatorRegistry.get("altsim")
 
-        assert sim1 == qdsim
-        assert qdsim == altsim
+    assert sim1 == qdsim
+    assert qdsim == altsim
 
-    def test_resolve_pin(self):
+def test_resolve_pin():
 
-        """Validates we can resolve a pin by name from the simulator registry"""
+    """Validates we can resolve a pin by name from the simulator registry"""
 
-        qdsim = QuantumDotSim("sim1")
-        drain = SimulatorRegistry.resolve_pin("sim1.drain")
+    qdsim = QuantumDotSim("sim1")
+    drain = SimulatorRegistry.resolve_pin("sim1.drain")
 
-        assert drain == qdsim.drain
+    assert drain == qdsim.drain
