7f4ca14abb099742310a99f0e1066549128134cc
jana-d <darulova.j@gmail.com>
2021-06-10 17:12:46 +0200

    Add gate and chip fixture

--- nanotune/tests/device/conftest.py
+++ nanotune/tests/device/conftest.py
@@ -1,4 +1,40 @@
 import pytest
+import pathlib
+from qcodes.tests.instrument_mocks import MockDAC as QcodesMockDAC
+from nanotune.drivers.mock_dac import MockDAC, MockDACChannel
+from nanotune.drivers.mock_readout_instruments import MockLockin, MockRF
+
+PARENT_DIR = pathlib.Path(__file__).parent.absolute()
+
+
+@pytest.fixture(scope="session")
+def qcodes_dac():
+    return QcodesMockDAC('qcodes_dac', num_channels=3)
+
+
+@pytest.fixture()
+def chip_config():
+    return os.path.join(PARENT_DIR, "chip.yml")
+
+
+@pytest.fixture()
+def device_on_chip(station, chip_config):
+    if hasattr(station, "device_on_chip"):
+        return station.device_on_chip
+
+    station.load_config_file(chip_config)
+    _chip = station.device_on_chip(station=station)
+    return _chip
+
+@pytest.fixture()
+def gate(station):
+    gate = DeviceChannel(
+        station.dac,
+        'top_barrier',
+        station.dac.ch01,
+        gate_id=0,
+    )
+    return gate
 
 
 @pytest.fixture(name="moc_dac_server")
@@ -11,8 +47,4 @@ def _make_mock_dac_server():
             pass
 
     dac = DACClient()
-    try:
-        yield dac
-    finally:
-        pass
-        # dac.close()
+    yield dac
