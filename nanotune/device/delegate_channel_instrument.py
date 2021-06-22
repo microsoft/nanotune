8f78ecc403b2c77217f948a323023bb7020a3071
jana-d <darulova.j@gmail.com>
2021-06-22 12:54:20 +0200

    Add changes made in qcodes

--- nanotune/device/delegate_channel_instrument.py
+++ nanotune/device/delegate_channel_instrument.py
@@ -97,94 +97,91 @@ class DelegateChannelInstrument(DelegateInstrument):
     def _create_and_add_channels(
         self,
         station: Station,
-        channels: Union[Mapping[str, Mapping[str, Any]], Mapping[str, str]],
+        channels: Mapping[str, Union[str, Mapping[str, Any]]],
     ) -> None:
-        """Add channels to the instrument.
-
-        .. code-block:: yaml
-
-        field:
-            type: qcodes.instrument.delegate.DelegateInstrument
-            init:
-            channels:
-                type: mypackage.mydevice.mydevice_channel.MyDeviceChannel
-                top_barrier:
-                  channel: dac.ch01
-                  my_input_param: 0
-
-        Args:
-            station: QCoDeS station, i.e. representation of the entire physical
-                setup.
-            channels: Mapping of names/aliases to instrument channels and
-                optional input parameters if a channel wrapper class is used to
-                instantiate the channel. If no 'type' field is given, the
-                channel is added as is using ``self.add_submodule``.
-
-        """
-
+        """Add channels to the instrument."""
         channel_wrapper = None
-        channels_dict: Dict[str, Union[str, Mapping[str, Any]]] = dict(channels)
-        channel_type = channels_dict.pop("type", None)
-        if channel_type is not None:
-            channel_type_elems = str(channel_type).split(".")
-            module_name = '.'.join(channel_type_elems[:-1])
-            instr_class_name = channel_type_elems[-1]
-            module = importlib.import_module(module_name)
-            channel_wrapper = getattr(module, instr_class_name)
+        chnnls_dict: Dict[str, Union[str, Mapping[str, Any]]] = dict(channels)
+        channel_type_global = chnnls_dict.pop("type", None)
+        if channel_type_global is not None and \
+           not isinstance(channel_type_global, str):
+            raise ValueError("Wrong channel type.")
+        channel_wrapper_global = _get_channel_wrapper_class(
+            channel_type_global
+        )
 
-        for param_name, input_params in channels_dict.items():
+        for channel_name, input_params in chnnls_dict.items():
+            if isinstance(input_params, Mapping):
+                input_params = dict(input_params)
+                channel_type_individual = input_params.pop("type", None)
+                channel_wrapper_individual = _get_channel_wrapper_class(
+                    channel_type_individual
+                )
+                if channel_wrapper_individual is None:
+                    channel_wrapper = channel_wrapper_global
+                else:
+                    channel_wrapper = channel_wrapper_individual
+            else:
+                channel_wrapper = channel_wrapper_global
+
             self._create_and_add_channel(
-                param_name=param_name,
+                channel_name=channel_name,
                 station=station,
                 input_params=input_params,
                 channel_wrapper=channel_wrapper,
-                )
+            )
 
     def _create_and_add_channel(
         self,
-        param_name: str,
+        channel_name: str,
         station: Station,
         input_params: Union[str, Mapping[str, Any]],
         channel_wrapper: Optional[Type[InstrumentChannel]],
-        **kwargs: Any
+        **kwargs: Any,
     ) -> None:
-        """Adds a channel to the instrument.
-
-        Args:
-            param_name: Alias/name of the channel.
-            station: QCoDeS' station containing the instrument containing the
-                channel.
-            input_params: Either the path to the channel or keyworded arguments
-                with 'channel' key containing the path of the channel and any
-                other input arguments taken by channel_wrapper.
-            channel_wrapper: Optional class to construct the channel. If none
-                given, the channel is added as is using ``self.add_submodule``.
-
-        """
-
+        """Adds a channel to the instrument."""
         if isinstance(input_params, str):
             try:
                 channel = self.parse_instrument_path(station, input_params)
-            except ValueError:
-                raise ValueError("Unknown channel path. Try: instrument.chXY")
+            except ValueError as v_err:
+                msg = "Unknown channel path."
+                raise ValueError(msg) from v_err
 
         elif isinstance(input_params, Mapping) and channel_wrapper is not None:
             channel = self.parse_instrument_path(
-                station, input_params['channel']
+                station, input_params["channel"]
             )
-            kwargs = dict(kwargs, **input_params)
+            wrapper_kwargs = dict(**kwargs, **input_params)
 
             channel = channel_wrapper(
-                channel.parent,
-                param_name,
-                **kwargs
+                parent=channel.parent,
+                name=channel_name,
+                **wrapper_kwargs
             )
         else:
-            raise ValueError('Unknown input type.')
+            raise ValueError(
+                "Channels can only be created from existing channels, "
+                "or using a wrapper channel class; "
+                f"instead got {input_params!r} inputs with "
+                f"{channel_wrapper!r} channel wrapper."
+            )
 
-        self.add_submodule(param_name, channel)
+        self.add_submodule(channel_name, channel)
 
 
     def __repr__(self) -> str:
         params = ", ".join(self.parameters.keys())
         return f"DelegateInstrument(name={self.name}, parameters={params})"
+
+def _get_channel_wrapper_class(
+    channel_type: Optional[str],
+) -> Optional[Type[InstrumentChannel]]:
+    """Get channel class from string specified in yaml."""
+    if channel_type is None:
+        return None
+    else:
+        channel_type_elems = str(channel_type).split(".")
+        module_name = ".".join(channel_type_elems[:-1])
+        instr_class_name = channel_type_elems[-1]
+        module = importlib.import_module(module_name)
+        return getattr(module, instr_class_name)
