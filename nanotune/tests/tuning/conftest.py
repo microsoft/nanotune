95613e73f514ab23a9841468c13463775ba559ec
jana-d <darulova.j@gmail.com>
2021-06-25 17:52:02 +0200

    Add mock pinchoff classifier to default input

--- nanotune/tests/tuning/conftest.py
+++ nanotune/tests/tuning/conftest.py
@@ -1,19 +1,20 @@
+from nanotune.tests.mock_classifier import MockClassifer
 import pytest
 from nanotune.tuningstages.settings import (DataSettings, SetpointSettings,
     Classifiers)
 
 @pytest.fixture(scope="function")
-def tuner_default_input(tmp_path, gate_1):
+def tuner_default_input(tmp_path):
     settings = {
         "name": "test_tuner",
         "data_settings": DataSettings(
             db_name="temp.db",
             db_folder=str(tmp_path),
         ),
-        "classifiers": Classifiers(),
+        "classifiers": Classifiers(pinchoff=MockClassifer(category="pinchoff")),
         "setpoint_settings": SetpointSettings(
             voltage_precision=0.001,
-            ranges_to_sweep=[(-0.3, 0)],
+            ranges_to_sweep=[(-1, 0)],
             safety_voltage_ranges=[(-3, 0)],
         ),
     }
