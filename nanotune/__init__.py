d538aed26a3ca444715e6ab94ecfeaedf676a198
jana-d <darulova.j@gmail.com>
2021-07-28 13:24:43 +0200

    remove git dependancy

--- nanotune/__init__.py
+++ nanotune/__init__.py
@@ -9,7 +9,5 @@ from nanotune.data.databases import *
 from nanotune.data.dataset import Dataset
 from nanotune.device.device import Device
 
-repo = git.Repo(path=os.path.dirname(nt.__file__), search_parent_directories=True)
-sha = repo.head.object.hexsha
-version = __version__  # repo.git.rev_parse(sha, short=8)
+version = __version__
 meta_tag = nt.config["core"]["meta_add_on"]
