ede7a30dcae5df6ac03426964d45b787721c9c29
jana-d <darulova.j@gmail.com>
2021-07-28 13:04:51 +0200

    update docs

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.490.g7174bd0.dirty',
+    VERSION: '0+untagged.499.g48fdd24',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
