4cedba1536643e13701d39923ef057784b66771a
jana-d <darulova.j@gmail.com>
2021-08-09 18:12:28 +0200

    Fix and clean up

--- nanotune/labelling/labellingtool.py
+++ nanotune/labelling/labellingtool.py
@@ -47,54 +47,19 @@ class LabellingTool(qtw.QMainWindow):
         if db_name is None:
             logger.warning("Labelling default main database.")
             db_name = nt.config["db_name"]
-        nt.set_database(db_name)
+        nt.set_database(db_name, db_folder)
         self.db_name = db_name
-
         self.db_folder = db_folder
 
-        print(qc.config['core']['db_location'])
         matplotlib.rc("font", size=figure_fontsize)
         super(LabellingTool, self).__init__()
 
         self.current_label = dict.fromkeys(LABELS, 0)
         self.experiment_id = experiment_id
 
-        if self.experiment_id is None:
-            # logger.error(
-            #     "Please select an experiment. Labelling entire "
-            #     + " database is not supported yet."
-            # )
-            # raise NotImplementedError
-            try:
-                (self._iterator_list,
-                self.labelled_ids,
-                self.n_total) = self.get_data_ids(start_over)
-            except IndexError as i:
-                print(i)
-                msg = "Did not find any unlabelled data in experiment "
-                qtw.QMessageBox.warning(
-                    self, "Error instantiating LabellingTool.", msg,
-                    qtw.QMessageBox.Ok
-                )
-        else:
-            try:
-                experiment = load_experiment(self.experiment_id)
-                runs = get_runs(experiment.conn, experiment.exp_id)
-                all_ids = [run['captured_run_id'] for run in runs]
-                if start_over:
-                    self._iterator_list = all_ids
-                    self.labelled_ids = []
-                    self.n_total = len(all_ids)
-                else:
-                    raise NotImplementedError
-
-            except ValueError as v:
-                msg = "Unable to load experiment."
-                print(v)
-                qtw.QMessageBox.warning(
-                    self, "Error instantiating LabellingTool.", msg,
-                    qtw.QMessageBox.Ok
-                )
+        (self._iterator_list,
+         self.labelled_ids,
+         self.n_total) = self.get_data_ids(start_over)
 
         self._id_iterator = iter(self._iterator_list)
         try:
@@ -103,7 +68,8 @@ class LabellingTool(qtw.QMainWindow):
             logger.warning(
                 "All data of this experiment is already labelled."
             )
-            raise
+            print("All data already labelled.")
+            return
 
         self._main_widget = qtw.QWidget(self)
         self.setCentralWidget(self._main_widget)
@@ -115,66 +81,52 @@ class LabellingTool(qtw.QMainWindow):
         start_over: bool = False,
     ) -> Tuple[List[int], List[int], int]:
         """"""
-        unlabelled_ids: List[int] = []
-        labelled_ids: List[int] = []
-        print("getting datasets")
 
-        # last_id = nt.get_last_dataid(self.db_name, db_folder=self.db_folder)
-        # all_ids = list(range(1, last_id))
-        all_ids = []
-        for experiment in experiments():
-            runs = get_runs(experiment.conn, experiment.exp_id)
-            all_ids += [run['captured_run_id'] for run in runs]
-
-        # dds = self.experiment.data_sets()
-        # if len(dds) == 0:
-        #     logger.error('Experiment has no data. Nothing to label.')
-        #     raise ValueError
-
-        # start_id = dds[0].run_id
-        # stop_id = dds[-1].run_id
-        # all_ids = list(range(start_id, stop_id+1))
-        print("len(all_ids): " + str(len(all_ids)))
-
-        # check if database has label columns as column
-        if not start_over:
-            # Make sure database has nanotune label columns. Just a check.
+        if self.experiment_id is None:
+            all_ids = []
+            for experiment in experiments():
+                runs = get_runs(experiment.conn, experiment.exp_id)
+                all_ids += [run['captured_run_id'] for run in runs]
+        else:
             try:
-
-                some_id = nt.get_last_dataid(self.db_name, self.db_folder)
-                ds = qc.load_by_run_spec(captured_run_id=some_id)
-                ds.get_metadata("good")
-            except OperationalError:
-                logger.warning(
-                    """No nanotune_label column found in current
-                                database. Probably because no data has been
-                                labelled yet. Hence starting over. """
+                experiment = load_experiment(self.experiment_id)
+                runs = get_runs(experiment.conn, experiment.exp_id)
+                all_ids = [run['captured_run_id'] for run in runs]
+            except ValueError as v:
+                msg = "Unable to load experiment."
+                print(v)
+                qtw.QMessageBox.warning(
+                    self, "Error instantiating LabellingTool.", msg,
+                    qtw.QMessageBox.Ok
                 )
-                start_over = True
-            # except RuntimeError:
-            #     logger.error('Probably data in experiment.')
-            #     raise
-        print("start_over: " + str(start_over))
+
         if start_over:
             unlabelled_ids = all_ids
             labelled_ids = []
         else:
-            unlabelled_ids = nt.get_unlabelled_ids(self.db_name)
-            labelled_ids = [x for x in all_ids if x not in unlabelled_ids]
+            unlabelled_ids_all = nt.get_unlabelled_ids(
+                self.db_name,
+                db_folder=self.db_folder,
+                get_run_id=False,
+            )
+            unlabelled_ids = []
+            labelled_ids = []
+            for run_id in all_ids:
+                if run_id in unlabelled_ids_all:
+                    unlabelled_ids.append(run_id)
+                else:
+                    labelled_ids.append(run_id)
 
         return unlabelled_ids, labelled_ids, len(all_ids)
 
     def initUI(self) -> None:
         """"""
-        # msg = str(len(self.labelled_ids)) + ' labelled, '
-        # msg += str(self.n_total - len(self.labelled_ids)) + ' to go.'
+
         self.progressbar = qtw.QProgressBar()
         self.progressbar.setMinimum(1)
         self.progressbar.setMaximum(100)
-        # self.progressbar.setTextVisible(True)
 
         self.statusBar().addPermanentWidget(self.progressbar)
-        # self.progressbar.setGeometry(30, 40, 200, 25)
 
         pp = len(self.labelled_ids) / self.n_total * 100
         self.progressbar.setValue(pp)
@@ -187,7 +139,7 @@ class LabellingTool(qtw.QMainWindow):
         # Y coordinate
         # Width of the frame
         # Height of the frame
-        self.setWindowTitle("nanotune Labelling Tool")
+        self.setWindowTitle("nanotune labelling tool")
 
         self._main_layout = self.initMainLayout()
         self._main_widget.setLayout(self._main_layout)
@@ -212,15 +164,7 @@ class LabellingTool(qtw.QMainWindow):
 
         while True:
             try:
-                # if self._cb[0] is not None:
-                #     _, self._cb = plot_by_id(self.current_id, axes=self._axes,
-                #                              colorbars=self._cb[0])
-                # else:
                 _, self._cb = plot_by_id(self.current_id, axes=self._axes)
-                # title = 'Run #{}  Experiment #{}'.format(self.current_id,
-                #                                          self.experiment_id)
-                # for ax in self._axes:
-                #     ax.set_title(title)
                 break
             except (RuntimeError, IndexError) as r:
                 logger.warning("Skipping current dataset" + str(r))
@@ -276,13 +220,10 @@ class LabellingTool(qtw.QMainWindow):
         LABELS_MAP = dict(nt.config["core"]["labels"])
         for label in LABELS:
             if label not in ["good"]:
-                # bl = QHBoxLayout()
+
                 btn = qtw.QPushButton(LABELS_MAP[label])
                 btn.setObjectName(label)
                 btn.setCheckable(True)
-                # bl.addWidget(btn)
-                # bl.addStretch(1)
-                # labels_column.addLayout(bl)
                 labels_column.addWidget(btn)
                 self._buttons.append(btn)
 
@@ -290,10 +231,6 @@ class LabellingTool(qtw.QMainWindow):
 
         # -----------   Finalize row   ----------- #
         finalize_row = qtw.QHBoxLayout()
-
-        # go_back_btn = QPushButton('Go Back')
-        # finalize_row.addWidget(go_back_btn)
-        # go_back_btn.clicked.connect(self.go_back)
 
         clear_btn = qtw.QPushButton("Clear")
         finalize_row.addWidget(clear_btn)
@@ -324,17 +261,14 @@ class LabellingTool(qtw.QMainWindow):
 
     def next(self) -> None:
         """"""
-        # TO DO: Loop to the next unlabelled dataset ...
         self._axes.clear()
         self._axes.relim()
-        # if len(self._figure.axes) > 1:
-        #     self._figure.delaxes(self._figure.axes[1])
+
         if self._cb[0] is not None:
             for cbar in self._cb:
                 cbar.ax.clear()
                 cbar.ax.relim()
                 cbar.remove()
-                # cbar = None
         self._figure.tight_layout()
 
         while True:
@@ -346,21 +280,8 @@ class LabellingTool(qtw.QMainWindow):
                 self.l1.setText("Plot ID: {}".format(self.current_id))
                 pp = len(self.labelled_ids) / self.n_total * 100
                 self.progressbar.setValue(pp)
-                # _, self._cb = plot_by_id(self.current_id, axes=self._axes,
-                #                              colorbars=self._cb[0])
-                # # if self._cb[0] is not None:
-                # _, self._cb = plot_by_id(self.current_id, axes=self._axes,
-                #                      colorbars=self._cb[0])
-                # else:
+
                 _, self._cb = plot_by_id(self.current_id, axes=self._axes)
-                # for cbar in self._cb:
-                #     cbar.ax.clear()
-                #     cbar.ax.relim()
-                #     cbar.remove()
-                # title = 'Run #{}  Experiment #{}'.format(self.current_id,
-                #                                          self.experiment_id)
-                # for ax in self._axes:
-                #     ax.set_title(title)
 
                 self._figure.tight_layout()
                 self._canvas.draw()
@@ -398,8 +319,7 @@ class LabellingTool(qtw.QMainWindow):
 
     def save_labels(self) -> None:
         """"""
-        # logger.error('Need to update label saving! -> One column per label.')
-        # raise NotImplementedError
+
         for button in self._buttons:
             if button.objectName() == label_bad:
                 continue
@@ -410,9 +330,9 @@ class LabellingTool(qtw.QMainWindow):
             msg = 'Please choose quality. \n \n Either "Good" or '
             msg += '"' + label_bad + '"' + " has"
             msg += " to be selected."
-            qtw.QMessageBox.warning(self, "Cannot save label.", msg, qtw.QMessageBox.Ok)
+            qtw.QMessageBox.warning(
+                self, "Cannot save label.", msg, qtw.QMessageBox.Ok)
         else:
-            # ds = load_by_id(self.current_id)
             ds = qc.load_by_run_spec(captured_run_id=self.current_id)
 
             for label, value in self.current_label.items():
@@ -421,49 +341,10 @@ class LabellingTool(qtw.QMainWindow):
             self.clear()
             self.next()
 
-    # def go_back(self):
-    #     """
-    #     """
-    #     self._axes.clear()
-    #     if self._cb[0] is not None:
-    #         self._cb[0].ax.clear()
-
-    #     while True:
-    #         try:
-    #             self.labelled_ids = self.labelled_ids[:-1]
-    #             self.current_id -= 1
-
-    #             # Update GUI
-    #             self.l1.setText('Plot ID: {}'.format(self.current_id))
-    #             pp = (len(self.labelled_ids)/self.n_total*100)
-    #             self.progressbar.setValue(pp)
-    #             if self._cb[0] is not None:
-    #                 _, self._cb = plot_by_id(self.current_id, axes=self._axes,
-    #                                          colorbars=self._cb[0])
-    #             else:
-    #                 _, self._cb = plot_by_id(self.current_id, axes=self._axes)
-    #             self._figure.tight_layout()
-
-    #             self._canvas.draw()
-    #             self._canvas.update()
-    #             self._canvas.flush_events()
-    #             break
-
-    #         except (RuntimeError, IndexError) as r:
-    #             logger.warning('Skipping a dataset ' +
-    #                             str(r))
-    #             # self.labelled_ids.append(self.current_id)
-    #             # self.current_id = self._id_iterator.__next__()
-    #             self.current_id -= 1
-    #             self.go_back()
-    #             pass
-
     def exit(self) -> None:
         """"""
-        # logger.warning('Saving labels.')
         self.n_total - len(self.labelled_ids)
-        quit_msg1 = "Please don't go"  # , nanotune needs you! \n"
-        # quit_msg1 += " " + str(n_missing) + ' datasets are calling for labels.'
+        quit_msg1 = "Please don't go"
         quit_msg2 = "Would you like to give it another try?"
         reply = qtw.QMessageBox.question(
             self, quit_msg1, quit_msg2, qtw.QMessageBox.Yes, qtw.QMessageBox.No
