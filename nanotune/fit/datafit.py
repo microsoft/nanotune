3a39d12e8c117046400998c72d04cb4a40c7757e
jana-d <darulova.j@gmail.com>
2021-08-16 09:45:04 +0200

    docstrings and move code where it belongs

--- nanotune/fit/datafit.py
+++ nanotune/fit/datafit.py
@@ -6,19 +6,32 @@ from typing import Any, Dict, List, Optional, Tuple
 import numpy.typing as npt
 import matplotlib
 import numpy as np
-import xarray as xr
 import qcodes as qc
-from qcodes.dataset.experiment_container import load_by_id
 
 import nanotune as nt
-from nanotune.data.dataset import (Dataset, default_coord_names,
-                                   default_readout_methods)
+from nanotune.data.dataset import default_coord_names
 
 logger = logging.getLogger(__name__)
 AxesTuple = Tuple[matplotlib.axes.Axes, matplotlib.colorbar.Colorbar]
 
 
-class DataFit(ABC, Dataset):
+class DataFit(ABC, nt.Dataset):
+    """Abstract base class for fitting classes.
+    It inherits from nt.Dataset to have access to post processed data as
+    necessary. The abstract `find_fit` and `plot_fit` methods, as well as
+    the `range_update_directives` property need to be
+    implemented by child classes. They are used by tuningstage classes.
+
+    Attributes:
+        features: features extracted by child classes.
+        range_update_directives: directives on how to update voltages other
+            than those swept in the current fit. Typically indicating general
+            directions such as more positive or negative, determined based
+            on the normalized signal strength. E.g. if the signal is too low, i.e.
+            the device is pinched off, this property will say that some other
+            voltages need to be set to more positive values.
+
+    """
     def __init__(
         self,
         qc_run_id: int,
@@ -30,7 +43,7 @@ class DataFit(ABC, Dataset):
         if db_folder is None:
             db_folder = nt.config["db_folder"]
 
-        Dataset.__init__(
+        nt.Dataset.__init__(
             self,
             qc_run_id,
             db_name,
@@ -42,22 +55,42 @@ class DataFit(ABC, Dataset):
 
     @property
     def features(self) -> Dict[str, Any]:
-        """"""
+        """Overwrites feature attribute of nt.Dataset class. Computes
+        features if they are not found.
+
+        Returns:
+            dict: mapping strings indicating the feature type to its value.
+        """
         if not self._features:
             self.find_fit()
         return self._features
 
+    @property
+    @abstractmethod
+    def range_update_directives(self) -> List[str]:
+        """Indicating general directions such as more positive or negative,
+        determined based on the normalized signal strength. E.g. if the signal
+        is too low, i.e. the device is pinched off, this property will say that
+        some other voltages need to be set to more positive values.
+
+        Returns:
+            list: list of directives.
+        """
+
     @abstractmethod
     def find_fit(self) -> None:
-        """"""
+        """Method to extract features. To be defined by child classes and
+        tailored to a specific measurement. It needs to populate self._features.
+        """
 
-    @property
-    def range_update_directives(self) -> List[str]:
-        """"""
-        return self._range_update_directives
+    @abstractmethod
+    def plot_fit(self) -> AxesTuple:
+        """Should display a plot fit important features shown."""
+        pass
+
 
     def save_features(self) -> None:
-        """"""
+        """Saves extracted features to QCoDeS metadata."""
         nt.set_database(self.db_name, db_folder=self.db_folder)
         ds = qc.load_by_run_spec(captured_run_id=self.qc_run_id)
         try:
@@ -66,86 +99,3 @@ class DataFit(ABC, Dataset):
             nt_meta = {}
         nt_meta["features"] = self.features
         ds.add_metadata(nt.meta_tag, json.dumps(nt_meta))
-
-    def get_edge(
-        self,
-        which_one: str,
-        readout_method: str = "transport",
-        delta_v: float = 0.05,
-        use_raw_data: bool = False,
-    ) -> npt.NDArray[np.float64]:
-        """"""
-        if self.dimensions[readout_method] != 2:
-            logger.error("get_edge method not implemented for 1D data.")
-            raise NotImplementedError
-        if use_raw_data:
-            data = self.raw_data[self.readout_methods[readout_method]]
-            coord_names = [str(it) for it in list(self.raw_data.coords)]
-        else:
-            data = self.data[readout_method]
-            coord_names = default_coord_names["voltage"]
-
-        signal = data.values
-        voltage_x = data[coord_names[0]].values
-        voltage_y = data[coord_names[1]].values
-
-        if which_one == "left vertical":
-            v_start = voltage_x[0]
-            dv = 0
-            didx = 1
-            for ii in range(1, len(voltage_x)):
-                dv += abs(v_start - voltage_x[ii])
-                v_start = voltage_x[ii]
-                if dv <= delta_v:
-                    didx = ii
-                else:
-                    break
-            edge = np.average(signal[:, 0 : didx + 1], axis=1)
-
-        elif which_one == "bottom horizontal":
-            v_start = voltage_y[0]
-            dv = 0
-            didx = 1
-            for ii in range(1, len(voltage_y)):
-                dv += abs(v_start - voltage_y[ii])
-                v_start = voltage_y[ii]
-                if dv <= delta_v:
-                    didx = ii
-                else:
-                    break
-            edge = np.average(signal[0 : didx + 1, :], axis=0)
-
-        elif which_one == "right vertical":
-            v_start = voltage_x[-1]
-            dv = 0
-            didx = 1
-            for ii in range(1, len(voltage_x)):
-                dv += abs(v_start - voltage_x[-ii])
-                v_start = voltage_x[-ii]
-                if dv <= delta_v:
-                    didx = ii
-                else:
-                    break
-            edge = np.average(signal[:, -(didx + 1) :], axis=1)
-
-        elif which_one == "top horizontal":
-            v_start = voltage_y[-1]
-            dv = 0
-            didx = 1
-            for ii in range(1, len(voltage_y)):
-                dv += abs(v_start - voltage_y[-ii])
-                v_start = voltage_y[-ii]
-                if dv <= delta_v:
-                    didx = ii
-                else:
-                    break
-            edge = np.average(signal[-(didx + 1) :, :], axis=0)
-
-        else:
-            logger.error("Trying to get an edge which does not exist:" + which_one)
-
-        return edge
-
-    @abstractmethod
-    def plot_fit(self) -> AxesTuple:
-        pass
